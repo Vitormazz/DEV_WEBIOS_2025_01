a68c73c8dd79b8c461bada247601dc219bd37b6f
Vitormazz <vitormazz12@gmail.com>
2025-06-03 21:40:56 -0300

    Inicio aula javascript

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,34 @@
+var idade
+// JavaScript
+console.log("Hello World") //apresentar uma mensagem
+console.clear() //limpar
+
+
+// Variaveis
+ var numero1 = 192 //inicializar uma variavel
+ console.log(numero1)
+ 
+
+ var num1 = 4
+ var num2 = 4
+
+ console.log(num1 + num2)
+
+
+ idade = 89 // iniciar a variavel em outra parte do codigo
+ console.log(idade)
+
+ // textos
+var nome1 = "Vitor Augusto"
+var nome2 = "Everton Santana"
+var nome3 = "123456789" // texto
+console.log(nome1)
+console.log(nome2)
+console.log(nome3)
+
+
+VAR // Flexivel e com menos regras (Posso alterar o valor inicial de uma var e recriar a mesma e outras partes dos codigo)
+
+LET // Voce não ppode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+CONST // Voce não pode recriar a mesma em outras partes do codigo e não pode alterar o valor inicial dela garantindo  confiança do dado que está sendo utilizado no decorrer do seu codigo  
